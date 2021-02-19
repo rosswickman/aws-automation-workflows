a12f1cc7752a0eecdb47122392867d5d0290b05f
Ross Wickman <ross@rwick.it>
2021-02-19 16:10:26 -0600

    remove tf sg

--- terraform/sg-public-web.tf
+++ terraform/sg-public-web.tf
@@ -11,32 +11,34 @@ terraform {
   }
 }
 
-resource "aws_security_group" "public_web" {
-  name        = "public-web-terraform"
-  description = "Public Web Access from Terraform"
-  vpc_id      = "vpc-bc441ec4"
+## Commented to remove Resource
 
-  ingress {
-    description = "Public HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# resource "aws_security_group" "public_web" {
+#   name        = "public-web-terraform"
+#   description = "Public Web Access from Terraform"
+#   vpc_id      = "vpc-bc441ec4"
 
-  ingress {
-    description = "Public HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Public HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Public HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+# }
