e1ae45d35e83141de77b6c3bdaa0aca26b6a38f3
Ross Wickman <ross@rwick.it>
2021-02-19 15:32:48 -0600

    replace tf sg template

--- terraform/sg-public-web.tf
+++ terraform/sg-public-web.tf
@@ -12,31 +12,31 @@ terraform {
 }
 
 resource "aws_security_group" "public_web" {
-  name        = "demo-public-web-tf"
-  description = "Public Web Access from Terraform"
+  name        = "public-web"
+  description = "Allow Public Web Traffic"
   vpc_id      = "vpc-bc441ec4"
-}
 
-resource "aws_security_group_rule" "allow_all" {
-  type              = "egress"
-  protocol          = "-1"
-  to_port           = 0
-  from_port         = 0
-  security_group_id = data.aws_security_group.public_web.id
-}
+  ingress {
+    description = "Public HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "port_80" {
-  type              = "ingress"
-  protocol          = "tcp"
-  to_port           = 80
-  from_port         = 80
-  security_group_id = data.aws_security_group.public_web.id
-}
+  ingress {
+    description = "Public HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "port_443" {
-  type              = "ingress"
-  protocol          = "tcp"
-  to_port           = 443
-  from_port         = 443
-  security_group_id = data.aws_security_group.public_web.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
---
+++ working/sg-public-web-multi.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "github-cicd-demo"
+    key    = "terrform/state/sg/demo-public-web"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_security_group" "public_web" {
+  name        = "demo-public-web-tf"
+  description = "Public Web Access from Terraform"
+  vpc_id      = "vpc-bc441ec4"
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  protocol          = "-1"
+  to_port           = 0
+  from_port         = 0
+  security_group_id = data.aws_security_group.public_web.id
+}
+
+resource "aws_security_group_rule" "port_80" {
+  type              = "ingress"
+  protocol          = "tcp"
+  to_port           = 80
+  from_port         = 80
+  security_group_id = data.aws_security_group.public_web.id
+}
+
+resource "aws_security_group_rule" "port_443" {
+  type              = "ingress"
+  protocol          = "tcp"
+  to_port           = 443
+  from_port         = 443
+  security_group_id = data.aws_security_group.public_web.id
+}
